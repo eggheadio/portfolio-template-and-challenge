6bbb2a12f14167ebe321623cd89444e38b919d60
Zac Jones <zacjones93@gmail.com>
2022-04-07 11:02:15 -0400

    update index with profile image photo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import Image from 'next/image'
 import type { PostType, ProjectType } from '../../types/post'
 import PostCard from '../components/PostCard'
 import ProjectCard from '../components/ProjectCard'
@@ -29,21 +30,24 @@ const Home: NextPage<{posts: PostType[], projects: ProjectType[]}> = ({posts, pr
         </div>
       </section>
 
-      <section className='container max-w-4xl mx-auto my-10 mt-20' >
+      <section className='container mx-auto my-10 mt-20 sm:max-w-4xl w-fit' >
         <div className='flex flex-col justify-center gap-4 mx-auto sm:flex-row w-fit'>
-          <div className="flex items-center self-center justify-center w-64 h-64 bg-gray-400 rounded-lg lg:w-96 lg:h-96 min-w-96">
-              <p>put an image of you here</p>
+        <div className='flex flex-col justify-between'>
+            <Image className="rounded-lg" height={384} width={384} src="/assets/erick-butler-xWGoK1tqxyo-unsplash.jpeg" />
+            <p className='text-sm text-gray-400'>
+              Photo by  <a href="https://unsplash.com/@erickwalterbutler?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Erick Butler</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </p>
           </div>
-          <div className='mt-4 sm:ml-4'>
+          <div className='sm:ml-4'>
             <Link href="/about">
               <a className="px-2 text-3xl font-bold underline transition-all duration-300 rounded cursor-pointer decoration-primary w-fit hover:ring-2 ring-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary hover:no-underline">
                   About Me
               </a>
             </Link>
-            <div className="mt-4 w-96">
+            <div className="mt-4 ">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
               </p>  
-              <p>
+              <p className='mt-2'>
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laboru.
               </p>
             </div>
